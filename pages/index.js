1c3f61dcfd4c8af82ac3553ac578108682622521
Lucy Heath <lucyheath07@gmail.com>
2023-04-28 21:04:22 +0100

    broken but getting there

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import {useState} from "react";
-
-
+import {DropDown} from "../components/dropDown";
 export default function Home()
 {
     
@@ -9,44 +8,18 @@ export default function Home()
     const toSay = "How are you doing?";
 
 
-async function bbb(event){
-    event.preventDefault();
-    try{
-        const response = await fetch("/api/createText", {
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json",
-            },
-            
-            body: JSON.stringify({p : toSay}),
-    
-        });
 
-        const data = await response.json();
-        if(response.status !== 200){
-            throw data.error || new Error(`reuqest failed with status code ${response.status}`);
 
-        }
-
-        setResult(data.result);
-    }catch(error){
-        console.error(error);
-        alert(error.message);
-    }
-}
 
     return (
         <div>
             <Head>
-                <title>Guess the message</title>
-                
+                <title>Guess the message</title> 
             </Head>
-
             <main>
+                <DropDown></DropDown>
                 <h2>Guess the message</h2>
-                <button onClick={bbb}>Hi</button>
-                <div>You said: {toSay}</div>
-            <div>Response is:{result}</div>
+               
             
             </main>
         </div>
